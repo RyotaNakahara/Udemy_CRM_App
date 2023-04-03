020d1ae39470aca738389a4920f249d4b7be4bfc
RyotaNakahara <rita.n4210@gmail.com>
2023-04-04 08:16:48 +0900

    hello

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Label from '@/components/InputLabel.vue' 
+import Input from '@/components/TextInput.vue' 
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+</script>
+
+<template>
+    <GuestLayout>
+        <Label value="件名">タイトル</Label>
+        <Input modelValue="初期値"></Input>
+    </GuestLayout>
+</template>
